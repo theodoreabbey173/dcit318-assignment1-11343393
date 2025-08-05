a4fe540a4296d100d517be84f7d4c0f2964891c0
Theodore Gyaqueh Abbey <theodoreabbey173@gmail.com>
2025-08-05 09:17:15 +0000

    Created the Grade Calculator App

---
+++ Grade Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class GradeCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter your numerical grade (0-100): ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+            Console.WriteLine("Your grade is: A");
+        else if (grade >= 80)
+            Console.WriteLine("Your grade is: B");
+        else if (grade >= 70)
+            Console.WriteLine("Your grade is: C");
+        else if (grade >= 60)
+            Console.WriteLine("Your grade is: D");
+        else
+            Console.WriteLine("Your grade is: F");
+    }
+}
+
